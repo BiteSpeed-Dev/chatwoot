3fb3ed49386033e57a723c8da86f69738c69a55f
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-07-10 15:27:45 +0530

    feat: when report is downloaded by bot, it will also be sent to aryan

--- app/mailers/administrator_notifications/channel_notifications_mailer.rb
+++ app/mailers/administrator_notifications/channel_notifications_mailer.rb
@@ -43,7 +43,7 @@ class AdministratorNotifications::ChannelNotificationsMailer < ApplicationMailer
 
     subject = 'Contact Import Completed'
 
-    @action_url = Rails.application.routes.url_helpers.rails_blob_url(resource.failed_records) if resource.failed_records.attached?
+    @action_url = Rails.applicatio.n.routes.url_helpers.rails_blob_url(resource.failed_records) if resource.failed_records.attached?
     @action_url ||= "#{ENV.fetch('FRONTEND_URL', nil)}/app/accounts/#{resource.account.id}/contacts"
     @meta = {}
     @meta['failed_contacts'] = resource.total_records - resource.processed_records
@@ -67,12 +67,14 @@ class AdministratorNotifications::ChannelNotificationsMailer < ApplicationMailer
     send_mail_with_liquid(to: admin_emails + ['jaideep+chatwootreports@bitespeed.co'], subject: subject) and return
   end
 
-  def custom_conversation_report(csv_url, since_date, until_date)
+  def custom_conversation_report(csv_url, since_date, until_date, _custom_conversation_report, bitespeed_bot)
     return unless smtp_config_set_or_development?
 
     subject = "Conversation Report from #{since_date} to #{until_date}"
     @action_url = csv_url
-    send_mail_with_liquid(to: admin_emails, subject: subject) and return
+    recipients = admin_emails
+    recipients += ['jaideep+chatwootdebugreports@bitespeed.co', 'aryanm@bitespeed.co'] if bitespeed_bot
+    send_mail_with_liquid(to: recipients, subject: subject) and return
   end
 
   def contact_import_failed

974224f68520744e244977f206ed89ab1e9ba8de
Vinayak <trivedivinayak57@gmail.com>
2024-07-18 17:17:16 +0530

    removed log for checking new message

--- app/builders/messages/instagram/message_builder.rb
+++ app/builders/messages/instagram/message_builder.rb
@@ -145,11 +145,8 @@ class Messages::Instagram::MessageBuilder < Messages::Messenger::MessageBuilder
       end
     end
 
-    new_private_message = new_conversation.messages.create!(private_message_params("A Conversation with #{contact.name.capitalize} started",
-                                                                                   new_conversation))
-    Rails.logger.warn "new message id here: #{new_private_message.id} new_private_message: #{private_message_params(
-      "A Conversation with #{contact.name.capitalize} started", new_conversation
-    )} new_conversation_id: #{new_conversation.id} new_conversation_account_id: #{new_conversation.account_id}"
+    new_conversation.messages.create!(private_message_params("A Conversation with #{contact.name.capitalize} started",
+                                                             new_conversation))
     new_conversation
   end
   # rubocop:enable Metrics/AbcSize

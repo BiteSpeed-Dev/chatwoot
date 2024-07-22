a7c19f5534bf98947a709973bf4496198cc28575
Vinayak <trivedivinayak57@gmail.com>
2024-07-22 21:21:55 +0530

    added job for sending custom agent reports

---
+++ app/jobs/agent_report_job.rb
@@ -0,0 +1,104 @@
+require 'json'
+require 'csv'
+
+class AgentReportJob < ApplicationJob
+  def generate_custom_report(account, range, params)
+    set_statement_timeout
+
+    current_date = Date.current
+
+    process_account(account, current_date, range, params, 'custom')
+  end
+
+  def set_statement_timeout
+    ActiveRecord::Base.connection.execute("SET statement_timeout = '60s'")
+  end
+
+  def report_builder(report_params)
+    V2::ReportBuilder.new(
+      Current.account,
+      report_params
+    )
+  end
+
+  def generate_readable_report_metrics(report_metric)
+    [
+      report_metric[:conversations_count],
+      Reports::TimeFormatPresenter.new(report_metric[:avg_first_response_time]).format,
+      Reports::TimeFormatPresenter.new(report_metric[:avg_resolution_time]).format,
+      Reports::TimeFormatPresenter.new(report_metric[:online_time]).format,
+      Reports::TimeFormatPresenter.new(report_metric[:busy_time]).format,
+      Reports::TimeFormatPresenter.new(report_metric[:reply_time]).format,
+      report_metric[:resolutions_count]
+    ]
+  end
+
+  def process_account(account, _current_date, range, params, frequency = 'daily')
+    report = generate_report(account, params)
+
+    if report.present?
+      Rails.logger.info "Data found for account_id: #{account.id}"
+
+      start_date = range[:since].strftime('%Y-%m-%d')
+      end_date = range[:until].strftime('%Y-%m-%d')
+
+      csv_content = generate_csv(report, start_date, end_date)
+      upload_csv(account.id, start_date, end_date, csv_content, frequency)
+    else
+      Rails.logger.info "No data found for account_id: #{account.id}"
+    end
+  end
+
+  def generate_report(account, params)
+    account.users.map do |agent|
+      agent_report = report_builder({ type: :agent, id: agent.id, since: params[:since], until: params[:until],
+                                      business_hours: ActiveModel::Type::Boolean.new.cast(params[:business_hours]) }).summary
+      [agent.name] + generate_readable_report_metrics(agent_report)
+    end
+  end
+
+  def generate_csv(results, start_date, end_date)
+    CSV.generate(headers: true) do |csv|
+      csv << ["Reporting period #{start_date} to #{end_date}"]
+      csv << []
+      csv << [
+        'Agent name', 'Assigned conversations', 'Avg first response time', 'Avg resolution time', 'Online time',
+        'Busy time', 'Avg customer waiting time', 'Resolution Count'
+      ]
+      results.each do |row|
+        csv << row
+      end
+    end
+  end
+
+  def upload_csv(account_id, start_date, end_date, csv_content, frequency)
+    # Determine the file name based on the frequency
+    file_name = "#{frequency}_agent_report_#{account_id}_#{end_date}.csv"
+
+    # For testing locally, uncomment below
+    # puts csv_content
+    # csv_url = file_name
+    # File.write(csv_url, csv_content)
+
+    # return;
+    # Upload csv_content via ActiveStorage and print the URL
+    blob = ActiveStorage::Blob.create_and_upload!(
+      io: StringIO.new(csv_content),
+      filename: file_name,
+      content_type: 'text/csv'
+    )
+
+    csv_url = Rails.application.routes.url_helpers.url_for(blob)
+
+    # Send email with the CSV URL
+    mailer = AdministratorNotifications::ChannelNotificationsMailer.with(account: Account.find(account_id))
+
+    if frequency == 'weekly'
+      mailer.weekly_conversation_report(csv_url, start_date, end_date).deliver_now
+    elsif frequency == 'daily'
+      mailer.daily_conversation_report(csv_url, end_date).deliver_now
+    else
+      mailer.custom_conversation_report(csv_url, start_date, end_date).deliver_now
+    end
+  end
+end

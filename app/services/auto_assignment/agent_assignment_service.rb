b18cd2e518847058ca59fbb0e9a232c2985bcfcc
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-08-14 10:41:54 +0530

    improved no online agents message

--- app/services/auto_assignment/agent_assignment_service.rb
+++ app/services/auto_assignment/agent_assignment_service.rb
@@ -17,7 +17,8 @@ class AutoAssignment::AgentAssignmentService
     if new_assignee.nil?
       sleep(5)
       Rails.logger.info "No agents were assigned, #{conversation.account_id}"
-      conversation.messages.create!(activity_message_params('Conversation not assigned to any agent as no agents were online'))
+      content = 'Conversation not assigned to any agent as no agents were online'
+      ::Conversations::ActivityMessageJob.perform_later(conversation, activity_message_params(content))
     else
       conversation.update(assignee: new_assignee)
     end

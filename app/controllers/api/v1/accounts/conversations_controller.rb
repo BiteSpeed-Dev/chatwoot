38f9146516efea3f44e4d302ea3935632e517159
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-09-07 08:41:19 +0530

    feat: move logic to job

--- app/controllers/api/v1/accounts/conversations_controller.rb
+++ app/controllers/api/v1/accounts/conversations_controller.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/ClassLength
 class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseController
   include Events::Types
   include DateRangeHelper
@@ -32,18 +31,20 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
 
   def create
     Rails.logger.info('Starting conversation creation process')
-    # previous_messages = fetch_previous_messages if params[:populate_historical_messages] == 'true'
+    previous_conversation = find_previous_conversation if params[:populate_historical_messages] == 'true'
 
-    # ActiveRecord::Base.transaction do
-    #   create_conversation_and_initial_message
-    #   populate_historical_messages(previous_messages) if params[:populate_historical_messages] == 'true'
-    # end
+    ActiveRecord::Base.transaction do
+      create_conversation_and_initial_message
+    end
+
+    if previous_conversation.present? && @conversation.present? && params[:populate_historical_messages] == 'true'
+      ConversationImport.new(@conversation.id,
+                             previous_conversation.id).perform_later
+    end
 
     Rails.logger.info('Completed conversation creation process:')
     @conversation = find_previous_conversation
     @conversation
-
-    # return success
   end
 
   def update
@@ -138,111 +139,12 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
     Rails.logger.info("Added initial message to conversation: #{@conversation.id}")
   end
 
-  # rubocop:disable Metrics/AbcSize
-  def populate_historical_messages(previous_messages)
-    Rails.logger.info("Populating historical messages for conversation: #{@conversation.id}")
-    previous_messages.each do |message_data|
-      new_message = @conversation.messages.create!(message_data[:message_attributes])
-      Rails.logger.info("Created historical message: #{new_message.id}")
-
-      message_data[:attachments].each do |attachment_data|
-        new_attachment = new_message.attachments.create!(attachment_data[:attributes])
-        Rails.logger.info("Created attachment for historical message: #{new_attachment.id}")
-
-        if attachment_data[:active_storage_data]
-          ActiveStorage::Attachment.create!(attachment_data[:active_storage_data].merge(record_id: new_attachment.id))
-          Rails.logger.info("Created ActiveStorage attachment for historical message attachment: #{new_attachment.id}")
-        end
-      end
-    end
-  end
-  # rubocop:enable Metrics/AbcSize
-
-  def fetch_previous_messages
-    Rails.logger.info('Fetching previous messages')
-    previous_conversation = find_previous_conversation
-
-    if previous_conversation.blank?
-      Rails.logger.info('No previous conversation found')
-      return []
-    end
-
-    Rails.logger.info("Found previous conversation: #{previous_conversation.id}")
-
-    messages = fetch_messages(previous_conversation)
-    Rails.logger.info("Processing #{messages.count} messages from previous conversation")
-
-    attachments = fetch_attachments_for_messages(messages)
-
-    build_message_data_with_attachments(messages, attachments)
-  end
-
   def find_previous_conversation
     Conversation.where(
       account_id: Current.account.id,
       inbox_id: params[:inbox_id],
       contact_id: params[:contact_id]
     ).order(created_at: :desc).first
-  end
-
-  def fetch_messages(conversation)
-    conversation.messages
-                .order(created_at: :asc)
-                .reject { |msg| msg.private && msg.content.include?('Conversation with') }
-  end
-
-  def fetch_attachments_for_messages(messages)
-    message_ids = messages.map(&:id)
-    Attachment.where(message_id: message_ids).group_by(&:message_id)
-  end
-
-  def build_message_data_with_attachments(messages, attachments)
-    messages.map do |message|
-      message_data = build_message_data(message)
-      message_attachments = attachments[message.id] || []
-      add_attachments_to_message_data(message_data, message_attachments)
-      message_data
-    end
-  end
-
-  def build_message_data(message)
-    Rails.logger.info("Building message data for message: #{message.id}")
-    {
-      message_attributes: message.attributes.except('id', 'conversation_id').merge(
-        additional_attributes: (message.additional_attributes || {}).merge(
-          ignore_automation_rules: true,
-          disable_notifications: true
-        )
-      ),
-      attachments: []
-    }
-  end
-
-  def add_attachments_to_message_data(message_data, attachments)
-    attachments.each do |attachment|
-      attachment_data = build_attachment_data(attachment)
-      message_data[:attachments] << attachment_data
-      Rails.logger.info("Processed attachment: #{attachment.id}")
-    end
-  end
-
-  def build_attachment_data(attachment)
-    attachment_data = { attributes: attachment.attributes.except('id', 'message_id') }
-    add_active_storage_data(attachment, attachment_data)
-    attachment_data
-  end
-
-  def add_active_storage_data(attachment, attachment_data)
-    Rails.logger.info("Adding ActiveStorage data for attachment: #{attachment.id}")
-    return unless attachment.file.attached?
-
-    attachment_data[:active_storage_data] = {
-      name: attachment.file.filename,
-      record_type: 'Attachment',
-      blob_id: attachment.file.blob.id,
-      created_at: Time.zone.now
-    }
-    Rails.logger.info("Added ActiveStorage data for attachment: #{attachment.id}")
   end
 
   def permitted_update_params
@@ -318,4 +220,3 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
 end
 
 Api::V1::Accounts::ConversationsController.prepend_mod_with('Api::V1::Accounts::ConversationsController')
-# rubocop:enable Metrics/ClassLength

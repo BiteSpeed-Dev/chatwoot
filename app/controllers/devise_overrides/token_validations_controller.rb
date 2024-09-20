109d0736622bafe7023c3c8a36ca9896e9f01637
Jaideep Guntupalli <guntupallijaideep@gmail.com>
2024-09-20 11:03:27 +0530

    feat: check billing status

--- app/controllers/devise_overrides/token_validations_controller.rb
+++ app/controllers/devise_overrides/token_validations_controller.rb
@@ -1,7 +1,12 @@
 class DeviseOverrides::TokenValidationsController < DeviseTokenAuth::TokenValidationsController
+  include BspdAccessHelper
+
   def validate_token
     # @resource will have been set by set_user_by_token concern
     if @resource
+      Rails.logger.info "Active Account ID: #{@resource.account_id}"
+      render_validate_token_error unless billing_status(@resource.account_id)
+
       render 'devise/token', formats: [:json]
     else
       render_validate_token_error
---
+++ app/helpers/bspd_access_helper.rb
@@ -0,0 +1,15 @@
+module BspdAccessHelper
+  def billing_status(active_account_id)
+    # api call to bitespeed to check billing status
+    # get api call to localhost:3001/csdb/auth/verify with accountId as query param
+    # if api call is successful, return true
+    # else return false
+
+    response = HTTParty.get("http://localhost:3003/csdb/auth/verify?accountId=#{active_account_id}")
+    Rails.logger.info "BSPD Access Helper: Billing status response - #{response}"
+    response.success?
+  rescue StandardError => e
+    Rails.logger.error "BSPD Access Helper: Error checking billing status - #{e.message}"
+    false
+  end
+end

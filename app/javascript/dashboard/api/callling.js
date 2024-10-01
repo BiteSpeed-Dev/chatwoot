8ead27fa52778896f9b6fa7b24a963733b495a6a
vinayak-trivedi <trivedivinayak57@gmail.com>
2024-10-01 19:21:34 +0530

    remove url exposed

--- app/javascript/dashboard/api/callling.js
+++ app/javascript/dashboard/api/callling.js
@@ -9,7 +9,6 @@ class CallingAPI extends ApiClient {
     formData.append('CallerId', callerId)
     formData.append('StatusCallback', 'http://localhost:3000')
     formData.append('StatusCallbackEvents', ['terminal', 'answered'])
-    // const url = `https://6e485a1bd4a92babac79988094fc3b21833df0db8d0afdfb:87bcb7510add042ad6fb60c7614544dc6c3b5afbef451cda@api.exotel.com/v1/Accounts/vinayak13/Calls/connect`
     const url = `https://${apiKey}:${token}${subDomain}/v1/Accounts/${sid}/Calls/connect`
     return axios.post(
       url,
